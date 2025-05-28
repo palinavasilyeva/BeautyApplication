55bd49a1708a983cb8674ad481b30356f33e3681
palinavasilyeva <palina.vasilyeva@microsoft.wsei.edu.pl>
2025-05-28 17:06:20 +0200

    Create folder View (Client and Master Dashbords)

---
+++ BeautyApplication/View/MasterDashboard.xaml.cs
@@ -0,0 +1,38 @@
+﻿using BeautyApplication.Data;
+using BeautyApplication.Models;
+using BeautyApplication.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Windows;
+
+namespace BeautyApplication.Views
+{
+    public partial class MasterDashboard : Window
+    {
+        private readonly SalonContext _context;
+        private readonly AppointmentService _appointmentService;
+        private readonly User _user;
+
+        public MasterDashboard(User user)
+        {
+            InitializeComponent();
+            _context = new SalonContext();
+            _appointmentService = new AppointmentService(_context);
+            _user = user;
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            var master = _context.Masters.FirstOrDefault(m => m.UserId == _user.UserId);
+            if (master != null)
+            {
+                AppointmentsListView.ItemsSource = _context.Appointments
+                    .Include(a => a.User)
+                    .Include(a => a.Service)
+                    .Where(a => a.MasterId == master.MasterId)
+                    .ToList();
+            }
+        }
+    }
+}

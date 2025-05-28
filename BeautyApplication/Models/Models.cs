2da1f8656f71b4dac7d8ba15f04ba8ed6703cf2c
palinavasilyeva <palina.vasilyeva@microsoft.wsei.edu.pl>
2025-05-28 16:23:53 +0200

    Class Models

--- BeautyApplication/Models/Models.cs
+++ BeautyApplication/Models/Models.cs
@@ -3,9 +3,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BeautyApplication.Models
 {
-    /// <summary>
-    /// Пользователь системы (клиент или мастер).
-    /// </summary>
     public class User
     {
         [Key]
